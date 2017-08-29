cc220ad36f02eb01fd6c8946985fd28b1037d536
Sean OConnor <soconnor@pharos.co.nz>
2017-08-29 15:40:29 +1200

    fix

--- UI/Components/ConsoleTextBox.cs
+++ UI/Components/ConsoleTextBox.cs
@@ -1,14 +1,13 @@
-﻿/*
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using amulware.Graphics;
-using TestClient.Meta;
-using TestClient.Library;
+using NLog;
+using System;
 
 namespace TestClient.UI.Components
 {
-    class ConsoleTextBox : TextBox<Logger.Entry>
+    class ConsoleTextBox : TextBox<string>
     {
-        private readonly List<Logger.Entry> entries = new List<Logger.Entry>(); 
+        private readonly List<string> entries = new List<string>(); 
         private readonly Logger logger;
 
         public ConsoleTextBox(Bounds bounds, Logger logger)
@@ -17,15 +16,17 @@ namespace TestClient.UI.Components
             this.logger = logger;
         }
         
-        protected override IReadOnlyList<Logger.Entry> GetItems()
+        protected override IReadOnlyList<string> GetItems()
         {
             entries.Clear();
-            logger.CopyRecentEntriesWithSeverity(lowestVisibleSeverity(), entries);
+            //logger.CopyRecentEntriesWithSeverity(lowestVisibleSeverity(), entries);
+            // TODO - retrieve entries. NLog listener?
             return entries;
         }
 
-        protected override (string, Color) Format(Logger.Entry item)
-            => (item.Text, colors[item.Severity]);
+        protected override Tuple<string, Color> Format(string item)
+        {
+            return new Tuple<string, Color>(item, Color.Red); // TODO - calc color
+        }
     }
 }
-*/
